2e827aae592fd650d8545cfb956fcab7a09c3161
Wesley Menezes <wesleymenezesdnovaes@gmail.com>
2024-08-30 22:23:24 -0300

    Inicial Commit

---
+++ program.js
@@ -0,0 +1,12 @@
+// 1 - Adicionar; => e menu (Lucas)
+// 2 - Editar; = > wesley
+// 3 - Remover; => Yasmin
+// 4 - Listar todos; => wesley
+// 5 - Obter via ID; => Daniel
+// 6 - Sair => lucas
+
+// npm i readline-sync
+//const prompt = require(" readline-sync");
+
+//Validação de entrada dos dados nos itens Editar, Remover, Obter via ID;
+//voltar (optcional)
